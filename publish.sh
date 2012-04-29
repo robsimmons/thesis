f795b8e8a6668a230f2fa8e4cf64c79e3f6931ed
Rob Simmons <robsimmons@gmail.com>
2012-04-29 19:41:43 -0400

    Modify publish script to write to pages.csv (yak shaving) and correct error Jason pointed out in chapter-foc.tex

--- publish.sh
+++ publish.sh
@@ -4,4 +4,6 @@ pdflatex thesis > /dev/null
 bibtex thesis
 pdflatex thesis > /dev/null
 pdflatex thesis > /dev/null
+date "+%Y-%m-%d %H:%M:%S,`gs -q -dNODISPLAY -c '(thesis.pdf) (r) file runpdfbegin pdfpagecount = quit'`" | cat pages.csv - > pages-tmp.csv
+mv pages-tmp.csv pages.csv
 cat to-publish | xargs ./one-publish.sh
