70698f105ad73a89d30008338148d3070f53e478
Rob Simmons <robsimmons@gmail.com>
2012-06-01 20:53:16 -0400

    Yak shaving - partial success, need more success

--- one-publish.sh
+++ one-publish.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-echo "Publishing $1"
-pdflatex $1 > /dev/null
-bibtex $1 > /dev/null
-pdflatex $1 > /dev/null
-pdflatex $1 > /dev/null
-scp $1.pdf typesafety.net:/home/www/www/thesis
+ALLSRC=introduction,chapter-foc,chapter-3-ordered,chapter-framework,chapter-correspondence,chapter-absmachine
+NUM=$1
+INPUT=$2
+UPLOAD=$3
+PAGENUM=`sed 's/{/ /g' < thesis.aux | sed 's/}/ /g' | awk "/$2/ { print \\$4 }"`
+
+
+
+if [ -n $UPLOAD ] 
+then
+  echo "Publishing Chapter $NUM (page $PAGENUM)"
+
+  # STEP 1: BIBTEX
+  sed -e "s/\\\\include/%/g" \
+      -e "s/%{$INPUT}/\\\\input{$INPUT}/g" \
+      < chapter-template.tex > chapter-$NUM.tex
+  pdflatex chapter-$NUM > /dev/null
+  bibtex chapter-$NUM # > /dev/null
+
+  # STEP 2: MAKE THE .aux FILE AWESOME
+  cat < chapter-template.tex > chapter-$NUM.tex
+  pdflatex chapter-$NUM > /dev/null
+else 
+  echo "Refreshing Chapter $NUM"
+fi
+
+sed -e "s/%INCLUDEONLY/\\\\includeonly{$INPUT}/g" \
+    -e "s/%CHAPTER/\\\\setcounter{chapter}{$((NUM-1))}/g" \
+    -e "s/%PAGE/\\\\setcounter{page}{$PAGENUM}/g" \
+     < chapter-template.tex > chapter-$NUM.tex
+pdflatex chapter-$NUM > /dev/null
+
+if [ -n $UPLOAD ]
+then
+  scp chapter-$NUM.pdf typesafety.net:/home/www/www/thesis
+fi
